e8261f951302512c22c5b0e82055f2c7f6e107f7
Jon Deaves <jondeaves@users.noreply.github.com>
2024-01-28 17:13:51 -0500

    Revert "Feature/rhyme styles"

--- app/constants/poem-styles.ts
+++ app/constants/poem-styles.ts
@@ -6,8 +6,6 @@ type ModalPoemStyles = {
   description: string;
   disabled: boolean;
   example?: string;
-  rhymingLines: Array<Array<number>>;
-  lineSyllables: Array<number | null> | null;
 };
 
 export const POEM_STYLES: ModalPoemStyles[] = [
@@ -17,11 +15,6 @@ export const POEM_STYLES: ModalPoemStyles[] = [
     description:
       "Rhymes and syllables galore! The first, second and fifth lines rhyme and must have the same number of syllables. The third and fourth lines also rhyme using a different word! ",
     disabled: false,
-    lineSyllables: [null, null, null, null, null],
-    rhymingLines: [
-      [1, 2, 5],
-      [3, 4],
-    ],
     example:
       "The beach is super duper great<br />But water in my eyes, I hate<br />Running full speed on the sand <br />Surfing with the entire band<br />The beach is super duper great",
   },
@@ -32,17 +25,7 @@ export const POEM_STYLES: ModalPoemStyles[] = [
       "Go all the way! Fourteen lines, ten syllables each. Here’s a sonnet’s rhyming scheme from the first line to the last: ABAB CDCD EFEF GG ",
     disabled: false,
     example:
-      "Shall I compare thee to a summer’s day?\nThou art more lovely and more temperate:\nRough winds do shake the darling buds of May,\nAnd summer’s lease hath all too short a date:\nSometime too hot the eye of heaven shines,\nAnd often is his gold complexion dimmed;\nAnd every fair from fair sometime declines,\n\nBy chance, or nature’s changing course, untrimmed:\nBut thy eternal summer shall not fade,\nNor lose possession of that fair thou ow’st;\nNor shall Death brag thou wander’st in his shade\nWhen in eternal lines to time thou grow’st:\nSo long as men can breathe or eyes can see,\nSo long lives this, and this gives life to thee.",
-    rhymingLines: [
-      [1, 3],
-      [2, 4],
-      [5, 7],
-      [6, 8],
-      [9, 11],
-      [10, 12],
-      [13, 14],
-    ],
-    lineSyllables: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+      "Shall I compare thee to a summer’s day?<br />Thou art more lovely and more temperate:<br />Rough winds do shake the darling buds of May,<br />And summer’s lease hath all too short a date:<br />Sometime too hot the eye of heaven shines,<br />And often is his gold complexion dimmed;<br />And every fair from fair sometime declines,<br /><br />By chance, or nature’s changing course, untrimmed:<br />But thy eternal summer shall not fade,<br />Nor lose possession of that fair thou ow’st;<br />Nor shall Death brag thou wander’st in his shade<br />When in eternal lines to time thou grow’st:<br />So long as men can breathe or eyes can see,<br />So long lives this, and this gives life to thee.",
   },
   {
     ident: "haiku",
@@ -50,17 +33,13 @@ export const POEM_STYLES: ModalPoemStyles[] = [
     description:
       "Poems of utmost precision. The first and last lines are five syllables and second line requires seven. ",
     disabled: false,
-    example: 
+    example:
       "Bones, aged a-creaky<br />Press snooze, press snooze, press again<br />Oh, the lumbago",
-    rhymingLines: [],
-    lineSyllables: [5, 5, 7],
   },
   {
     ident: "free",
     title: "FREE VERSE",
     description: "Write your poem however you see fit!",
     disabled: false,
-    rhymingLines: [],
-    lineSyllables: null,
   },
 ];
